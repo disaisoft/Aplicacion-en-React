28e9b19f3ab511d384892027c7dfda6ba945ee59
disaisoft <disaisoft@gmail.com>
2019-07-17 09:33:00 -0500

    actualizacion app

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const About = function(){
+    return <h1>Hola mundo Geek!</h1>
+}
+
+const AboutDos = () => {
+    return <h1>Hola mundo Geek!</h1>
+}
+
+//Retornar una funcion de manera explicita
+const AboutTres = () => (<h1>Hola mundo Geek!</h1>);
+
+export default About;
